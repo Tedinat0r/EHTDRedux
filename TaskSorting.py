a7546d899461e0d4bc124340bf37667ed07e2d59
Teddy <teddyjek98@gmail.com>
2025-04-14 17:35:17 +0100

    Committed Changes from March

--- TaskSorting.py
+++ TaskSorting.py
@@ -1,18 +1,15 @@
-
-
-
-
-
-
-
 def sorting_dict_struct(dictionary):
     sorting_dict = {}
     completed_tasks = {}
     for item in list(dictionary.keys()):
         if dictionary[item].complete != True:
-            sorting_dict.__setitem__(item, [dictionary[item].priority,dictionary[item].urgency])
+            sorting_dict.__setitem__(item, [dictionary[item].priority,dictionary[item].urgency, dictionary[item].energy, dictionary[item].energy_quant])
+            for thing in sorting_dict.keys():
+                print(thing)
         else:
             completed_tasks.__setitem__(item, [dictionary[item].priority, dictionary[item].urgency])
+    for thing in sorting_dict.keys():
+        print(thing)
     return sorting_dict, completed_tasks
 
 
@@ -31,3 +28,19 @@ def task_sort(dictionary):
         item = sorted(item, key= lambda x: x[1][1], reverse=True)
     return(sorted_items)
 
+
+def energy_sort(task_array, type):
+    sorted_by_energy = []
+    for item in task_array:
+        of_type = []
+        not_type = []
+        for element in item:
+            if element[1][2] != type:
+                not_type.append(element)
+            else:
+                of_type.append(element)
+        of_type = sorted(of_type, key = lambda x: x[1][3], reverse=True)
+        not_type = sorted(not_type, key=lambda x: x[1][3], reverse=True)
+        final = of_type + not_type
+        sorted_by_energy.append(final)
+    return sorted_by_energy
